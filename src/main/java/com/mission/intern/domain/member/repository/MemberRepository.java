5e7fb96b6ab0cecd28be5faddc136967bedd2041
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-08-09 18:37:50 +0900

    :sparkles: 시큐리티 아키텍처를 보완할 인터페이스의 구현체를 구현한다.
    - UserDetails를 반환할 UserDetailService를 구현한다.
    - 인가 처리 실패를 위한 AccessDeniedHandler 구현체를 구현한다.
    - 인증 처리 실패를 위한 AuthenticationEntryPoint 구현체를 구현한다.
    - username을 통해 UserDetails를 반환할 UserDetailService를 구현한다.

--- src/main/java/com/mission/intern/domain/member/repository/MemberRepository.java
+++ src/main/java/com/mission/intern/domain/member/repository/MemberRepository.java
@@ -2,6 +2,9 @@ package com.mission.intern.domain.member.repository;
 
 import com.mission.intern.domain.member.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository {
     Member save(Member member);
+    Optional<Member> findByUsername(String username);
 }
